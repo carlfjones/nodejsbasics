d4b253e67af20c9feffec6eb9cf3931a4673c9c4
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2020-04-26 13:18:37 +0100

    Nodemon installed

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+    res.setHeader("Content-Type", "text/html");
+    res.send("Hello")
+});
+
 app.listen(300, (e) => {
     if (e) throw e;
     console.log('Server is running on 3000');
